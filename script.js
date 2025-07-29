430241f66209646b55ba1ee8563f056eaf295e1e
NotAlex575 <dapontealessandro89gmail.com>
2025-07-29 14:09:24 +0200

    importato hobbies.js in scripts.js,  ottenimento del valore hobby tramite la function hobbies (che contiene i 3 valori richiesti)

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const person = require("./modules/names.js")
+const hobbies = require("./modules/hobbies.js")
 
-const persona = person("Paolo", "Paoletti")
+const persona = person("Paolo", "Paoletti");
+const hobby = hobbies("Videogiochi", "Nuoto", "Mangiare"); 
 console.log(persona);
+console.log(hobby)
